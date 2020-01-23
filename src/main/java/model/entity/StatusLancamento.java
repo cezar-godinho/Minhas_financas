399d87d4d5971959260540a85f39dce2ded63d54
Cezar Godinho <cezar.godinho@fatec.sp.gov.br>
2020-01-23 13:24:24 -0300

    Add Program

---
+++ src/main/java/model/entity/StatusLancamento.java
@@ -0,0 +1,9 @@
+package model.entity;
+
+public enum StatusLancamento {
+
+    PENDENTE,
+    CANCELADO,
+    EFETIVADO
+
+}
---
+++ src/main/java/model/entity/Usuario.java
@@ -0,0 +1,31 @@
+package model.entity;
+import lombok.*;
+
+import javax.persistence.*;
+import java.util.Objects;
+
+@Entity
+@Table(name="usuario", schema = "financas")
+@Builder
+@Data
+public class Usuario {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "nome")
+    private String nome;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "senha")
+    private String senha;
+
+
+
+
+
+}
